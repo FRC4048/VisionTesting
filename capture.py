d8c5c5ca4e852f203b946429faa1ee3eb28ba2d4
benkrugman <krugmanb21@student,westboroughk12.org>
2018-10-16 20:12:03 -0400

    program now finds the target

--- capture.py
+++ capture.py
@@ -1,6 +1,7 @@
 import cv2	
 import os
 import math
+import numpy
 from grip import GripPipeline
 
 #Aspect ratio (microsoft)
@@ -13,6 +14,10 @@ VRES = 360
 
 #Target dimentions
 TWIDTH_CM = 25
+
+#return codes
+ERROR = -1
+SUCCESS = 0
 
 #file number
 i = 1
@@ -37,11 +42,11 @@ def find_fov(ha, va, dfov):
 	return hf
 
 #Horizontal FOV
-def find_distance(hf):
-	
-	x1,y1,w1,h1 = cv2.boundingRect(pipeline.find_contours_output[0])
-	x2,y2,w2,h2 = cv2.boundingRect(pipeline.find_contours_output[1])
-	
+def find_distance(hf, cnt1, cnt2):
+	#clean this up we dont want this to happen twice (once in find_target)
+	x1,y1,w1,h1 = cv2.boundingRect(cnt1)
+	x2,y2,w2,h2 = cv2.boundingRect(cnt2)
+
 	Tpx=abs(x2-x1)+(w1/2)+(w2/2)
 	#print("Tpx =" + str(Tpx))
 	fovcm = (TWIDTH_CM * HRES)/Tpx
@@ -51,12 +56,32 @@ def find_distance(hf):
 	
 	return distance
 
+#may change this
+def find_target():
+	rc = ERROR
+	cnt1 = 0
+	cnt2 = 0
+	if len(pipeline.find_contours_output) >= 2:
+#		cnt1 = max(pipeline.find_contours_output, key = cv2.contourArea)
+#		pipeline.find_contours_output.remove(cnt1)
+#		cnt2 = max(pipeline.find_contours_output, key = cv2.contourArea)
+		largest_contours = sorted(pipeline.find_contours_output, key=cv2.contourArea) [-2:]		
+		cnt1 = largest_contours[0]
+		cnt2 = largest_contours[1]
 
+		x1,y1,w1,h1 = cv2.boundingRect(cnt1)
+		x2,y2,w2,h2 = cv2.boundingRect(cnt2)
+		#print("width = " + str(float(w1)/float(w2)) + " Height = " + str(float(h1)/float(h2)))
+		if (float(h1)/float(h2) >= 0.8 and float(h1)/float(h2) <= 1.2) and (float(w1)/float(w2) >= 0.8 and float(w1)/float(w2) <= 1.2):
+			rc = SUCCESS
+	return cnt1, cnt2, rc
 
 #Configuring the camera to look the way we want	
 config_microsoft_cam()
 M_HFOV = find_fov(M_HA, M_VA, M_DFOV)
 print(M_HFOV)
+
+
 pipeline = GripPipeline()
 
 #Video Capture stuff
@@ -68,6 +93,7 @@ while(True):
 	s, im = cam.read() # captures image
 	cv2.imshow("Test Picture", im) # displays captured image	
 	pipeline.process(im) 
+	cnt1, cnt2, rc = find_target()
 	event = cv2.waitKey(25) & 0xFF
 	if event == ord('c'):
 		cv2.imwrite("./microsoft"+str(i)+".jpg", im)
@@ -76,8 +102,8 @@ while(True):
 	elif event == ord('p'):
 		print(len(pipeline.find_contours_output))
 		print
-	elif event == ord('d') and len(pipeline.find_contours_output) == 2:
-		find_distance(M_HFOV)
+	elif event == ord('d') and rc == SUCCESS:
+		find_distance(M_HFOV, cnt1, cnt2)
 	elif event == ord('q'):
 		break 
 
