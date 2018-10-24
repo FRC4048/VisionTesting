aa619da94b714e07719122238f9e4315ba770cc3
benkrugman <krugmanb21@student,westboroughk12.org>
2018-10-24 19:46:51 -0400

    Angle calculations

--- capture.py
+++ capture.py
@@ -2,7 +2,12 @@ import cv2
 import os
 import math
 import numpy
+import time
+import socket
 from grip import GripPipeline
+
+from networktables import NetworkTables
+import logging 
 
 #Aspect ratio (microsoft)
 M_HA = 16
@@ -10,8 +15,7 @@ M_VA = 9
 M_DFOV = 68.5
 
 HRES = 640
-VRES = 360
-
+VRES = (M_VA * HRES)/M_HA #360
 #Target dimentions
 TWIDTH_CM = 25
 
@@ -22,10 +26,10 @@ SUCCESS = 0
 #file number
 i = 1
 
-def config_microsoft_cam():
-	#os.system("v4l2-ctl -v --set-fmt-video=width=1920,height=1080")
-	#os.system("v4l2-ctl --set-fmt-video=width=1920,height=1080,pixelformat=YUYV")
+printTime = False
+printStat = False
 
+def config_microsoft_cam():
 	os.system("v4l2-ctl --set-ctrl=exposure_auto=1")
 	os.system("v4l2-ctl --set-ctrl=exposure_absolute=5")
 	os.system("v4l2-ctl --set-ctrl=brightness=30")
@@ -34,15 +38,40 @@ def config_microsoft_cam():
 	os.system("v4l2-ctl --set-ctrl=white_balance_temperature_auto=0")
 	os.system("v4l2-ctl --set-ctrl=white_balance_temperature=9000")
 
+def init_network_tables():
+	logging.basicConfig(level=logging.DEBUG)
+	NetworkTables.enableVerboseLogging()
+	NetworkTables.initialize(server='10.12.1.59')
+		
+	while (not NetworkTables.isConnected()):
+		time.sleep(1)
+		print("waiting")
+
+
+def init_UDP_client():
+	UDP_IP = "172.20.10.2"	
+	#UDP_IP  = "10.12.1.59"
+	UDP_PORT = 5005
+	MESSAGE = "Hello world"
+	MESSAGE = MESSAGE*4
+	print "UDP target IP:", UDP_IP
+	print "UDP target port:", UDP_PORT
+	print "message:", MESSAGE
+	
+	sock = socket.socket(socket.AF_INET, #internet
+					      socket.SOCK_DGRAM) #UDP
+	sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
 #ha --> horizontal aspect, va --> vertical aspect, dfov --> diagonal FOV
 #These formulas are found on: vrguy.blogspot.com/2013/04/converting-diagonal-field-of-view-and.html
 def find_fov(ha, va, dfov):
-	da = math.sqrt(ha*ha + va*va)
+	da = math.sqrt(ha*ha + va*va)	
 	hf = math.degrees(math.atan(math.tan(math.radians(dfov/2))*(ha/da))*2)
 	return hf
 
 #Horizontal FOV
 def find_distance(hf, cnt1, cnt2):
+	global printStat
 	#clean this up we dont want this to happen twice (once in find_target)
 	x1,y1,w1,h1 = cv2.boundingRect(cnt1)
 	x2,y2,w2,h2 = cv2.boundingRect(cnt2)
@@ -52,9 +81,27 @@ def find_distance(hf, cnt1, cnt2):
 	fovcm = (TWIDTH_CM * HRES)/Tpx
 	#print("fovcm = "+ str(fovcm))
 	distance = fovcm/(2*math.tan(math.radians(hf/2)))		
-	print("distance = " + str(distance))
 	
+	if printStat: 
+		print("Object 1 Height = " + str(h1) + "Width = " + str(w1) + "X, Y = " + str(x1) + ","  + str(y1)) 
+		print("Object 2 Height = " + str(h2) + "Width = " + str(w2) + "X, Y = " + str(x2) + ","  + str(y2)) 
+		printStat = False
+
 	return distance
+
+def find_angle(hf, cnt1, cnt2):
+	global HRES
+	
+	x1,y1,w1,h1 = cv2.boundingRect(cnt1)
+	x2,y2,w2,h2 = cv2.boundingRect(cnt2)
+	
+	Dpx = (HRES)/(2*math.tan(math.radians(hf/2)))
+	targetMidpoint = (x1+x2)/2		
+	offsetLength =  targetMidpoint - (HRES/2) 	
+	
+	offsetAngle = math.degrees(math.atan(offsetLength/Dpx))
+	
+	return offsetAngle
 
 #may change this
 def find_target():
@@ -71,15 +118,17 @@ def find_target():
 
 		x1,y1,w1,h1 = cv2.boundingRect(cnt1)
 		x2,y2,w2,h2 = cv2.boundingRect(cnt2)
-		#print("width = " + str(float(w1)/float(w2)) + " Height = " + str(float(h1)/float(h2)))
+
 		if (float(h1)/float(h2) >= 0.8 and float(h1)/float(h2) <= 1.2) and (float(w1)/float(w2) >= 0.8 and float(w1)/float(w2) <= 1.2):
 			rc = SUCCESS
 	return cnt1, cnt2, rc
 
 #Configuring the camera to look the way we want	
 config_microsoft_cam()
+#init_UDP_client()
+#init_network_tables()
 M_HFOV = find_fov(M_HA, M_VA, M_DFOV)
-print(M_HFOV)
+#print(M_HFOV)
 
 
 pipeline = GripPipeline()
@@ -90,10 +139,19 @@ cam = cv2.VideoCapture(0)
 cam.set(3, HRES)
 cam.set(4, VRES)
 while(True):
+	captureStartTime = cv2.getTickCount()
 	s, im = cam.read() # captures image
+	captureEndTime = cv2.getTickCount()
+	captureTime = (captureEndTime - captureStartTime)/cv2.getTickFrequency()
 	cv2.imshow("Test Picture", im) # displays captured image	
+	processStartTime = cv2.getTickCount()
 	pipeline.process(im) 
-	cnt1, cnt2, rc = find_target()
+	processEndTime = cv2.getTickCount()
+	cnt1, cnt2, rc = find_target()                
+	processTime = (processEndTime - processStartTime)/cv2.getTickFrequency()
+	if printTime:
+		print("Frame processing time:" +  str(processTime) + "\nFrame Capture Time:" + str(captureTime))
+                                     	
 	event = cv2.waitKey(25) & 0xFF
 	if event == ord('c'):
 		cv2.imwrite("./microsoft"+str(i)+".jpg", im)
@@ -103,7 +161,13 @@ while(True):
 		print(len(pipeline.find_contours_output))
 		print
 	elif event == ord('d') and rc == SUCCESS:
-		find_distance(M_HFOV, cnt1, cnt2)
+		distance = find_distance(M_HFOV, cnt1, cnt2)
+		print("distance = " + str(distance))
+	elif event == ord('s'):
+		printStat = True
+	elif event == ord('a'):
+		angle = find_angle(M_HFOV, cnt1, cnt2)
+		print("angle = " + str(angle))
 	elif event == ord('q'):
 		break 
 
